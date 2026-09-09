015f03db67c0965bf19d7024c3344e8923ea6ba1
NiklasGi <niklas.gittel@gmail.com>
2026-09-09 18:28:56 +0200

    refactor: update intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { Mail } from "lucide-react"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
@@ -11,13 +12,17 @@ export default function Page() {
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>Let's see if the pipeline is working correctly.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
+          <h1 className="text-2xl font-bold">Hey there!</h1>
+          <p className="text-muted-foreground">I love to build stuff and write code. Currently looking for a start-up or project to pour my heart into.</p>
+          <div className="flex flex-row gap-2 mt-2">
+            <Button render={<a href="mailto:niklas@gittel.dev" />} nativeButton={false}>
+              <Mail />
+              Get in touch
+            </Button>
+            <Button variant="outline">
+              View projects
+            </Button>
+          </div>
         </div>
       </div>
     </div>
