7b434751136fc5dcf00d92915f5ebdf1f887a58b
NiklasGi <niklas.gittel@gmail.com>
2026-09-21 13:11:57 +0200

    feat: update work section

--- lib/sections/work/workExperiences.ts
+++ lib/sections/work/workExperiences.ts
@@ -11,6 +11,19 @@ const workExperiences: WorkExperience[] = [
         ]
     },
     {
+        title: "Business Critical Incident (BCI) Manager",
+        company: "Festo SE & Co. KG",
+        date: "Jan 2026 - Present",
+        details: [{ detail: "First person of contact in case of BCIs." }, { detail: "Responsible for coordinating team efforts and communication with stake holders while solving BCIs." }]
+    },
+    {
+        title: "Website Quality & Governance Lead",
+        company: "Festo SE & Co. KG",
+        date: "Jan 2025 - Present",
+        details: [
+            { detail: "Conducted website audits across SEO, Security, Accessibility, and Compliance for all Festo websites." }, { detail: "Automated processes and built a fleet of automated scanners." }, { detail: "Managed and automated the findings and follow-up actions." }]
+    },
+    {
         title: "Software Engineer",
         company: "Festo SE & Co. KG",
         date: "Oct 2022 - Present",
@@ -27,29 +40,16 @@ const workExperiences: WorkExperience[] = [
                     ]
             },
             { detail: "Scaled operations internationally by hiring and onboarding a team of four, bringing the new location to full productivity within two months." },
-{ detail: "Co-responsible for Festo’s CDN (Akamai)" },
-{ detail: "Set up  CI/CD pipelines and automated testing for multiple applications" }
+            { detail: "Co-responsible for Festo’s CDN (Akamai)" },
+            { detail: "Set up  CI/CD pipelines and automated testing for multiple applications" }
         ]
     },
-{
-    title: "Website Quality & Governance Lead",
+    {
+        title: "Agile Master",
         company: "Festo SE & Co. KG",
-            date: "Jan 2025 - Present",
-                details: [
-                    { detail: "Conducted website audits across SEO, Security, Accessibility, and Compliance for all Festo websites." }, { detail: "Automated processes and built a fleet of automated scanners." }, { detail: "Managed and automated the findings and follow-up actions." }]
-},
-{
-    title: "Business Critical Incident (BCI) Manager",
-        company: "Festo SE & Co. KG",
-            date: "Jan 2026 - Present",
-                details: [{ detail: "First person of contact in case of BCIs." }, { detail: "Responsible for coordinating team efforts and communication with stake holders while solving BCIs." }]
-},
-{
-    title: "Agile Master",
-        company: "Festo SE & Co. KG",
-            date: "Jan 2023 - Dec 2025",
-                details: [{ detail: "Coached and coordinated the team in the context of the SAFe framework." }, { detail: "Assumed Release Train Engineer responsibilities." }]
-}
+        date: "Jan 2023 - Dec 2025",
+        details: [{ detail: "Coached and coordinated the team in the context of the SAFe framework." }, { detail: "Assumed Release Train Engineer responsibilities." }]
+    }
 ];
 
 export default workExperiences;
