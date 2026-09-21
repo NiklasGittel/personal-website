61d826e358a464b7b2bfaac2ca0410b5034efde4
NiklasGi <niklas.gittel@gmail.com>
2026-09-21 09:06:51 +0200

    bug: fix outdated git link

--- components/FooterContent.tsx
+++ components/FooterContent.tsx
@@ -9,7 +9,7 @@ export const FooterContent = () => {
         <Button size="icon" variant="ghost" render={<a href="https://linkedin.com/in/niklas-gittel" target="_blank" rel="noreferrer" />} nativeButton={false}>
           <FaLinkedin className="h-5 w-5" />
         </Button>
-        <Button size="icon" variant="ghost" render={<a href="https://github.com/NiklasGi" target="_blank" rel="noreferrer" />} nativeButton={false}> 
+        <Button size="icon" variant="ghost" render={<a href="https://github.com/NiklasGittel" target="_blank" rel="noreferrer" />} nativeButton={false}> 
           <FaGithub className="h-5 w-5" />
         </Button>
       </div>
